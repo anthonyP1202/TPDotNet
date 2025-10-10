c67ebfd97b3a38efb36af204b4d1a6dfa2c54b3f
anthonyP1202 <pereiraanthony20@orange.fr>
2025-10-10 10:31:07 +0100

    controller with error

--- StacktimSolution/StacktimApi/Controllers/PlayerController.cs
+++ StacktimSolution/StacktimApi/Controllers/PlayerController.cs
@@ -4,6 +4,7 @@ using StacktimApi.DTOs;
 using StacktimApi.Model;
 using System;
 using System.Collections.Immutable;
+using System.Collections.ObjectModel;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -30,28 +31,68 @@ namespace StacktimApi.Controllers
 
         // GET api/<PlayerController>/5
         [HttpGet("{id}")]
-        public Player Get(int id)
+        public ActionResult<Player> Get(int id)
         {
             Player player = _context.Players.FirstOrDefault(play=>play.Id == id);
+            if (player == null)
+            {
+                return NotFound();
+            }
             return player;
         }
 
         // POST api/<PlayerController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post([FromBody] Player value)
         {
+            Player player = new Player();
+            player.Email = value.Email;
+            player.Teams = value.Teams;
+            player.Rank = value.Rank;
+            player.Pseudo = value.Pseudo;
+            _context.Players.Add(player);
         }
 
         // PUT api/<PlayerController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public ActionResult<Player> Put(int id, [FromBody] Player value)
         {
+            Player player = _context.Players.FirstOrDefault(play => play.Id == id);
+            if (player == null)
+            {
+                return NotFound();
+            }
+            player.Email = value.Email;
+            player.Rank = value.Rank;
+            player.Pseudo = value.Pseudo;
+            
+            ICollection<Team> teams = player.Teams.ToList();
+            foreach (Team team in value.Teams.ToList())
+            {
+                if (teams.Contains(team)){
+                    continue;
+                } else
+                {
+                    teams.Add(team);
+                }
+            }
+            player.Teams = teams;
+
+            _context.Players.Update(player);
+            return player;
         }
 
         // DELETE api/<PlayerController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public ActionResult<Player> Delete(int id)
         {
+            Player player = _context.Players.FirstOrDefault(play => play.Id == id);
+            if (player == null)
+            {
+                return NotFound();
+            }
+            _context.Players.Remove(player);
+            return player;
         }
     }
 }
