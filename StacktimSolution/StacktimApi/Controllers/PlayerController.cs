eb23407a3b026260910d6c8cd2239634425dbb7d
anthonyP1202 <pereiraanthony20@orange.fr>
2025-10-10 14:06:58 +0100

    tested request need to check adding teams

--- StacktimSolution/StacktimApi/Controllers/PlayerController.cs
+++ StacktimSolution/StacktimApi/Controllers/PlayerController.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using StacktimApi.Data;
 using StacktimApi.DTOs;
 using StacktimApi.Model;
 using System;
 using System.Collections.Immutable;
 using System.Collections.ObjectModel;
+using System.Diagnostics.Metrics;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -23,34 +26,52 @@ namespace StacktimApi.Controllers
 
         // GET: api/<PlayerController>
         [HttpGet]
-        public IEnumerable<Player> Get()
+        public IEnumerable<PlayerDTO> Get()
         {
-            IEnumerable<Player> player = _context.Players.ToList();
-            return player;
+            IEnumerable<Player> players = _context.Players.ToList();
+
+            List<PlayerDTO> playerDTO = new List<PlayerDTO> { };
+            
+            foreach(Player player in players)
+            {
+                playerDTO.Add(convertToDTO(player));
+            }
+            
+            IEnumerable<PlayerDTO> iPlayerDTO = playerDTO;
+            return iPlayerDTO;
         }
 
         // GET api/<PlayerController>/5
         [HttpGet("{id}")]
-        public ActionResult<Player> Get(int id)
+        public ActionResult<PlayerDTO> Get(int id)
         {
             Player player = _context.Players.FirstOrDefault(play=>play.Id == id);
             if (player == null)
             {
                 return NotFound();
             }
-            return player;
+            return convertToDTO(player);
         }
 
         // POST api/<PlayerController>
         [HttpPost]
-        public void Post([FromBody] Player value)
+        public ActionResult<PlayerDTO> Post([FromBody] Player value)
         {
             Player player = new Player();
             player.Email = value.Email;
             player.Teams = value.Teams;
             player.Rank = value.Rank;
             player.Pseudo = value.Pseudo;
-            _context.Players.Add(player);
+            try
+            {
+                _context.Players.Add(player);
+                _context.SaveChanges();
+            } catch (Exception err)
+            {
+                Console.WriteLine(err);
+                return NotFound();
+            }
+            return Ok();
         }
 
         // PUT api/<PlayerController>/5
@@ -67,18 +88,25 @@ namespace StacktimApi.Controllers
             player.Pseudo = value.Pseudo;
             
             ICollection<Team> teams = player.Teams.ToList();
-            foreach (Team team in value.Teams.ToList())
+            if (value.Teams.ToList() != null)
             {
-                if (teams.Contains(team)){
-                    continue;
-                } else
+                foreach (Team team in value.Teams.ToList())
                 {
-                    teams.Add(team);
+                    if (teams.Contains(team))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        teams.Add(team);
+                    }
                 }
             }
+            
             player.Teams = teams;
 
             _context.Players.Update(player);
+            _context.SaveChanges();
             return player;
         }
 
@@ -92,7 +120,60 @@ namespace StacktimApi.Controllers
                 return NotFound();
             }
             _context.Players.Remove(player);
-            return player;
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpGet("leaderboard")]
+        public ActionResult<IEnumerable<PlayerDTO>> LeaderBoard()
+        {
+            IEnumerable<Player> players = _context.Players.FromSqlRaw("SELECT TOP 10 * FROM Players ORDER BY \"TotalScore\" desc ").ToList();
+            List<PlayerDTO> playerDTO = new List<PlayerDTO> { };
+
+            foreach (Player player in players)
+            {
+                playerDTO.Add(convertToDTO(player));
+            }
+
+            IEnumerable<PlayerDTO> iPlayerDTO = playerDTO;
+            return Ok(iPlayerDTO);
+        }
+
+        private static PlayerDTO convertToDTO(Player player)
+        {
+            Ranks playerRanking = 0;
+            switch (player.Rank)
+            {
+                case "Bronze":
+                    playerRanking = Ranks.Bronze;
+                    break;
+                case "Silver":
+                    playerRanking = Ranks.Silver;
+                    break;
+                case "Gold":
+                    playerRanking = Ranks.Gold;
+                    break;
+                case "Platinum":
+                    playerRanking = Ranks.Platinum;
+                    break;
+                case "Diamond":
+                    playerRanking = Ranks.Diamond;
+                    break;
+                case "Master":
+                    playerRanking = Ranks.Master;
+                    break;
+            }
+
+            PlayerDTO playerDTO = new PlayerDTO();
+            playerDTO.Id = player.Id;
+            playerDTO.Teams = player.Teams.ToList();
+            playerDTO.Email = player.Email;
+            playerDTO.Rank = playerRanking;
+            playerDTO.TotalScore = player.TotalScore;
+            playerDTO.Pseudo = player.Pseudo;
+
+            return playerDTO;
         }
     }
 }
