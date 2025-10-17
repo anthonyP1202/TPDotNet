1a6fda1d80813ce72561232540bf32097ab21462
anthonyP1202 <144995617+anthonyP1202@users.noreply.github.com>
2025-10-17 19:41:13 +0200

    GetPlayers_ReturnsAllPlayers and GetPlayer_WithValidId_ReturnsPlayer theoricaly working

---
+++ StacktimSolution/StacktimApi.Tests/Controller/PlayersControllerTests.cs
@@ -0,0 +1,74 @@
+﻿using IdentityModel.OidcClient;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StacktimApi.Controllers;
+using StacktimApi.Data;
+using StacktimApi.DTOs;
+using StacktimApi.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacktimApi.Tests.Controller
+{
+    public class PlayersControllerTests
+    {
+
+        private readonly DbContextOptions<StacktimDbContext> _options;
+        public PlayersControllerTests() {
+            DbContextOptionsBuilder<StacktimApi.Data.StacktimDbContext> builder = new DbContextOptionsBuilder<StacktimApi.Data.StacktimDbContext>();
+            builder.UseInMemoryDatabase("StacktimDb");
+            _options = builder.Options;
+        }
+
+        [Fact]
+        public void GetPlayers_ReturnsAllPlayers()
+        {
+            using (StacktimApi.Data.StacktimDbContext context = new StacktimApi.Data.StacktimDbContext(_options))
+            {
+                //Arrange
+                IEnumerable<Player> users = context.Players.ToList();
+                PlayerController playerController = new PlayerController(context);
+
+                //Act
+                IEnumerable<PlayerDTO> usersResponse = playerController.Get();
+
+                //Assert
+                Assert.Equal(users.Count(), usersResponse.Count());
+            }
+        }
+
+        [Fact]
+        public void GetPlayer_WithValidId_ReturnsPlayer()
+        {
+            using (StacktimApi.Data.StacktimDbContext context = new StacktimApi.Data.StacktimDbContext(_options))
+            {
+                //Arrange
+                PlayerController playerController = new PlayerController(context);
+                Player player = context.Players.FirstOrDefault();
+                if (player == null)
+                {
+                    List<Player> players = new List<Player>
+                    {
+                        new Player { Email = "testting@test.com", Pseudo = "testting", Rank = "Gold", TotalScore = 0 },
+                        new Player { Email = "outofidea'sbrother@test.com", Pseudo = "outofidea'sbrother", Rank = "Silver", TotalScore = 0 }
+                    };
+
+                    context.AddRange(players);
+                    context.SaveChanges();
+                }
+                player = context.Players.First();
+
+                //Act
+                ActionResult<PlayerDTO> playerToTest = playerController.Get(player.Id);
+
+                //Test
+                var okResult = Assert.IsType<OkObjectResult>(playerToTest.Result);
+                PlayerDTO playerDto = Assert.IsType<PlayerDTO>(okResult.Value);
+                Assert.Equal(player.Id, playerDto.Id);
+            }
+        }
+    }
+}
--- StacktimSolution/StacktimApi/Controllers/PlayerController.cs
+++ StacktimSolution/StacktimApi/Controllers/PlayerController.cs
@@ -46,11 +46,12 @@ namespace StacktimApi.Controllers
         public ActionResult<PlayerDTO> Get(int id)
         {
             Player player = _context.Players.FirstOrDefault(play=>play.Id == id);
+            Console.WriteLine(player.Id);
             if (player == null)
             {
                 return NotFound();
             }
-            return convertToDTO(player);
+            return Ok(convertToDTO(player));
         }
 
         // POST api/<PlayerController>
