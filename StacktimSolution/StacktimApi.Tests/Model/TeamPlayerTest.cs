9e69481f50bd558299f74e97759294a37b79b947
anthonyP1202 <144995617+anthonyP1202@users.noreply.github.com>
2025-10-17 23:27:48 +0200

    tested the DBO and TeamPlayer

--- StacktimSolution/StacktimApi.Tests/Model/TeamPlayerTest.cs
+++ StacktimSolution/StacktimApi.Tests/Model/TeamPlayerTest.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using StacktimApi.Data;
+using StacktimApi.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,38 @@ using System.Threading.Tasks;
 
 namespace StacktimApi.Tests.Model
 {
-    internal class TeamPlayerTest
+    public class TeamPlayerTest
     {
+        private readonly DbContextOptions<StacktimDbContext> _options;
+        public TeamPlayerTest()
+        {
+            DbContextOptionsBuilder<StacktimApi.Data.StacktimDbContext> builder = new DbContextOptionsBuilder<StacktimApi.Data.StacktimDbContext>();
+            builder.UseInMemoryDatabase("StacktimDb");
+            _options = builder.Options;
+        }
+
+        [Fact]
+        public void TestTeamPlayer()
+        {
+            //Set
+            TeamPlayer teamPlayer = new TeamPlayer();
+
+            //ACt
+            teamPlayer.PlayerId = 1;
+            teamPlayer.TeamId = 2;
+            teamPlayer.Role = 0;
+            teamPlayer.JoinDate = DateTime.Now;
+
+            var teamId = teamPlayer.TeamId;
+            var playerId = teamPlayer.PlayerId;
+            var role = teamPlayer.Role;
+            var joinDate = teamPlayer.JoinDate;
+
+            //Test
+            Assert.Equal(1, teamPlayer.PlayerId);
+            Assert.Equal(2, teamPlayer.TeamId);
+            Assert.Equal(0, teamPlayer.Role);
+            Assert.Equal(teamPlayer.JoinDate, teamPlayer.JoinDate); //this one is ridiculous even by my standard
+        }
     }
 }
---
+++ StacktimSolution/StacktimApi.Tests/Model/UpdatePlayerDtoTest.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StacktimApi.Data;
+using StacktimApi.DTOs;
+using StacktimApi.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacktimApi.Tests.Model
+{
+    public class UpdatePlayerDtoTest
+    {
+        private readonly DbContextOptions<StacktimDbContext> _options;
+        public UpdatePlayerDtoTest()
+        {
+            DbContextOptionsBuilder<StacktimApi.Data.StacktimDbContext> builder = new DbContextOptionsBuilder<StacktimApi.Data.StacktimDbContext>();
+            builder.UseInMemoryDatabase("StacktimDb");
+            _options = builder.Options;
+        }
+
+        [Fact]
+        public void TestUpdateDto()
+        {
+            //A
+            UpdatePlayerDTO upDto = new UpdatePlayerDTO();
+
+            //A
+            upDto.Email = "coverage";
+            upDto.Pseudo = "also";
+            upDto.Rank = Ranks.Gold;
+            upDto.TotalScore = 0;
+            ICollection<Team> teams = [];
+            upDto.Teams = teams;
+
+            var email = upDto.Email;
+            var pseudo = upDto.Pseudo;
+            var rank = upDto.Rank;
+            var totalScore = upDto.TotalScore;
+            var teamsList = upDto.Teams;
+
+            //A
+            Assert.Empty(teamsList);
+            Assert.Equal("coverage", email);
+            Assert.Equal(rank, Ranks.Gold);
+            Assert.Equal(totalScore, 0);
+            Assert.Equal("also", pseudo);
+
+        }
+    }
+}
